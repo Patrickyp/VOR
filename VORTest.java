4ae0a043bda8733018d13f85ff4965ceb8569452
Patrickyp <Patrickyp@users.noreply.github.com>
2014-04-07 14:31:00 -1000

    Update VORTest.java

--- VORTest.java
+++ VORTest.java
@@ -6,7 +6,15 @@ import org.junit.Test;
 public class VORTest {
 
 	@Test
-	public void test() {
+	public void testNewSignal() {
+		fail("Not yet implemented");
+	}
+	
+	public void testCalcRadial() {
+		fail("Not yet implemented");
+	}
+	
+	public void testCalcDirection(){
 		fail("Not yet implemented");
 	}
 
