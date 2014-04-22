07f27f752d254f4fe35a2216d6b4f933ee3e7513
conitsuk <conitsuk@hawaii.edu>
2014-04-21 20:49:29 -1000

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -1,10 +1,12 @@
-public class Radio{
+import java.util.Random;
 
+public class Radio{
     
     int radial;
-    int code;
+    String code;
     boolean signal;
-    
+    Random random = new Random();
+    String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
     
     public Radio (int x) {
         
@@ -14,7 +16,14 @@ public class Radio{
         return this.radial;
     }
     
-    public int station_code() {
+    public String station_code() {
+        int temp = 0;
+        StringBuilder sb = new StringBuilder();
+    	for(int i = 0; i < 3; i++) {
+        	temp = random.nextInt(letters.length - 1);
+        	sb.append(letters[temp]);
+        }
+    	code = sb.toString();
         return this.code;
     }
     
