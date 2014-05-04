ab04d57d78c5e111344e4feb4a215085a3ff31d0
conitsuk <conitsuk@hawaii.edu>
2014-05-04 11:55:15 -1000

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -4,20 +4,22 @@ public class Radio{
     
     int radial;
     String code;
-    boolean signal;
+    boolean signal, station;
     Random random = new Random();
     String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    StringBuilder sb;
+    
     
     public Radio (int x) {
         
     }
 
-    public int radial() {
+    public int getRadial() {
     	
         return this.radial;
     }
     
-    public String station_code() {
+    public String get_Station() {
         int temp = 0;
         StringBuilder sb = new StringBuilder();
     	for(int i = 0; i < 3; i++) {
@@ -26,6 +28,14 @@ public class Radio{
         }
     	code = sb.toString();
         return this.code;
+    }
+    
+    public String get_Station() {
+        return station;
+    }
+    
+    public boolean over_Station() {
+        return overstation;
     }
     
     public boolean signal_status() {
