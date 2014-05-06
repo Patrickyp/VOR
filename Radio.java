4b2f9038335a9cc14d5777b7e2db2f0d67b184fc
Patrickyp <Patrickyp@users.noreply.github.com>
2014-05-05 15:31:58 -1000

    Update Radio.java

--- Radio.java
+++ Radio.java
@@ -1,62 +1,54 @@
+/**
+ * Simulated Radio class.
+ */
 import java.util.Random;
 
 public class Radio {
-    
-    int radial;
-    String station;
-    boolean signal;	// Variable that determines wheter or not your over a station.
-    Random random = new Random();
-    String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    StringBuilder sb;
-    
-    
-    public Radio (boolean timed) {
-        this(false);
-    }
-
-    public int generate_Radial() {
-        this.radial = random.nextInt(360);
-    }
-
-    public String generate_Station() {
-        int temp = 0;
-        sb = new StringBuilder();
-    	for(int i = 0; i < 3; i++) {
-        	temp = random.nextInt(letters.length - 1);
-        	sb.append(letters[temp]);
-        }
-    	station = sb.toString();
-        return this.station;
-    }
-    
-    public void reset() {
-        generate_Radial();
-        generate_Station();
-        gen_Random_OverStation();
-    }
-    
-	public int gen_RandomInt(int min, int max) {
-		return (int)(Math.random() * (max - min + 1)) + min;
+	int seed;
+	int badCounter = 5;
+	int ID1 = seed + 767;
+	int ID2 = seed + 321;
+	double startRad = 0;
+	public Radio(int seed){
+		this.seed = seed;
+	}
+	public double getRadial(){
+		double radial;
+		Random dice = new Random();
+		radial = dice.nextDouble() * 3.45;
+		int tick = dice.nextInt(2);
+		if(tick > 0){
+			return startRad + radial;
+		}
+		return startRad + 360 - radial;
 	}
 	
-	/**
-	 * Determines if you are currently over radial station 
-	 **/
-	 
-	private void gen_Random_OverStation() {
-		this.signal = (gen_RandomInt(0, 23) == 0);
+	public int getID(){
+		Random rnd = new Random();
+		int a = rnd.nextInt(2);
+		if(a == 0){
+			return this.ID1;
+		}
+		return ID2;
 	}
-    
-    public int getRadial() {
-        return this.radial;
-    }
-    
-    public String get_Station() {
-        return station;
-    }
-    
-    public boolean over_Station() {
-        return signal;
-    }
-    
+	
+	public String getSigQual(){
+		if(badCounter <= 0){
+			return "";
+		}
+		if(this.badCounter < 5){
+			this.badCounter--;
+			//System.out.println("bad counter = " + this.badCounter);
+			return "bad";
+		}
+		
+		Random rn4 = new Random();
+		int a = rn4.nextInt(3);
+		if(a == 0){
+			this.badCounter--;
+			//System.out.println("bad counter = " + this.badCounter);
+			return "bad";
+		}
+		return "";
+	}
 }
